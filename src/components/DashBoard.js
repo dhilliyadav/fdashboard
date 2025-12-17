b05d87f3c0975e5dd413d47b9a2b835be770025b
dhilliyadav <dhilliyadav07@gmail.com>
2025-12-17 17:03:09 +0530

    projects

---
+++ src/components/DashBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../src/App.css';
+function DashBoard() {
+    return (
+        <>
+        <div className='dashboard-container'>
+            <h1>DashBoard Component</h1>
+            <p>This is the DashBoard component content.</p>
+        </div>
+        </>
+    );
+}
+export default DashBoard;
